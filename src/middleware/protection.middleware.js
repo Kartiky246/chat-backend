6545e082271507bc01cdeee6f50de8e0bc604e1d
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-02-14 22:52:48 +0530

    protection route added

---
+++ src/middleware/protection.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+const protection = async (req, res, next) => {
+  try {
+    const token = req.cookies.accessToken;
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    req.user = await User.findById(decoded.id).select("-password");
+
+    next();
+  } catch (error) {
+    console.log("issue in token", error);
+    throw new Error("Not authorized, token failed");
+  }
+};
+
+export default protection;
