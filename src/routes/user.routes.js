5ba6ad7737ee6b7b4ce3420edcdfbdf4cdc41496
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-02-15 06:53:15 +0530

    search user route added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,9 +5,15 @@ const router = Router();
 import { upload } from "../middleware/multer.middleware.js";
 
 // importing the controllers
-import { login, register } from "../controllers/user.controller.js";
+import {
+  findUserWithName,
+  login,
+  register,
+} from "../controllers/user.controller.js";
+import protection from "../middleware/protection.middleware.js";
 
 router.route("/register").post(upload.single("avatar"), register);
 router.route("/login").post(login);
+router.route("/searchUser").get(protection, findUserWithName);
 
 export default router;
