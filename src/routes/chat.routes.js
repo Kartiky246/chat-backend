403e8cb705fe1f8434fdebd9aa38b658426d13bc
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-02-23 20:14:11 +0530

    route changed

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -5,12 +5,16 @@ const router = Router();
 import { upload } from "../middleware/multer.middleware.js";
 
 // importing the controllers
-import { sendMessage } from "../controllers/chat.controller.js";
+import { accessChat } from "../controllers/chat.controller.js";
 
 // middleware
 import protection from "../middleware/protection.middleware.js";
 import validateUsers from "../middleware/validateUsers.middleware.js";
 
-router.route("/chatting").post([protection, validateUsers, sendMessage]);
+router.route("/").post([protection, validateUsers, accessChat]);
+// router.route("/group").post(protection, createGroupChat);
+// router.route("/changename").put(protection, changename);
+// router.route("/groupremove").put(protection, removeFromGroup);
+// router.route("/groupadd").put(protection, addToGroup);
 
 export default router;
