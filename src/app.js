4434ad26c63038c3a2dc286ec0d5a8301d9393ed
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-02-26 21:42:00 +0530

    message api added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import { errorHandler, notFound } from "./misc/error.middleware.js";
 // import router
 import userRouter from "./routes/user.routes.js";
 import chatRouter from "./routes/chat.routes.js";
+import messageRouter from "./routes/message.routes.js";
 // middleware
 app.use(
   cors({
@@ -20,6 +21,7 @@ app.use(cookieParser());
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/chat", chatRouter);
+app.use("/api/v1/message", messageRouter);
 
 app.use(notFound);
 app.use(errorHandler);
