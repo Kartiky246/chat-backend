74de2bb904ed39a6df341f08919a208cfa80f12d
kartiky246 <kartiky246@gmail.com>
2024-03-14 00:14:51 +0530

    changes

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
+
 const connectDB = async () => {
+  console.log("PP00",process.env.MONGODB_URL,process.port)
   try {
     const connection = await mongoose.connect(`${process.env.MONGODB_URL}`);
     console.log("connected to database");
