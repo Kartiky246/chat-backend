be6cb788f96fe7baa16332734a9de20cbec7fd78
kartiky246 <kartiky246@gmail.com>
2024-03-14 20:37:14 +0530

    fixes8

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(
     credentials: true})
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+})
+
+
+
 
 app.use(expres.json());
 app.use(expres.urlencoded({ extended: true }));
