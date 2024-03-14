42d937737d25397964673aceb7726f9f86fe2f7d
kartiky246 <kartiky246@gmail.com>
2024-03-14 20:42:30 +0530

    debugging

--- src/middleware/protection.middleware.js
+++ src/middleware/protection.middleware.js
@@ -4,8 +4,11 @@ const protection = async (req, res, next) => {
   try {
     const token = req.cookies.accessToken;
 
+    console.log("COOKIES",token)
+    console.log("env variable",process.env.ACCESS_TOKEN_SECRET)
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
+    console.log("DEC",decoded)
     req.user = await User.findById(decoded.id).select("-password");
     next();
   } catch (error) {
